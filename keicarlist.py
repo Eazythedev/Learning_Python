8748d7f81d53f5f067892afde11c14469cd94bdd
Eazythedev <111327362+Eazythedev@users.noreply.github.com>
2026-02-05 05:13:22 -0800

    Create keicarlist.py
    A function that returns a list of kei car models.

---
+++ keicarlist.py
@@ -0,0 +1,6 @@
+def kei_cars():
+    return ["Honda N-Box", "Suzuki Hustler", "Daihatsu Copen"]
+common_kei_car_models = kei_cars()
+print(common_kei_car_models[0])
+print(common_kei_car_models[1])
+print(common_kei_car_models[2])
