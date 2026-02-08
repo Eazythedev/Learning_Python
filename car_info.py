5e0f46499e702a4e536905c9f8b298fc6768bf3c
Eazythedev <111327362+Eazythedev@users.noreply.github.com>
2026-02-07 20:38:15 -0800

    Update car_info.py

--- car_info.py
+++ car_info.py
@@ -1,6 +1,6 @@
 # A function that holds a dictionary with information for a car unpacked with **
 # Created by Ezaiah Akyeem / Eazythedev / Eruine
-# Date: February 6, 2026
+# Date: February 7, 2026
 
 def the_function_2(make, model):
     print("This car is a", make, model)
