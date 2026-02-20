fda0d0b56675bb7f0b5271f13f78582037c974a2
Eazy <111327362+Eazythedev@users.noreply.github.com>
2026-02-19 20:39:10 -0800

    Create rallygamepy.py

---
+++ rallygamepy.py
@@ -0,0 +1,10 @@
+# Module
+
+def greeting(player_name):
+    print("Welcome, " + player_name + " to Rally World WRC")
+
+car1 = {
+    "Make": "Toyota",
+    "Model": "Celica",
+    "Model_Year": "1993"
+}
