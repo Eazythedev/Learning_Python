78ab7ae816f1492c203af8af86847be9bd86bc4e
Eazy <111327362+Eazythedev@users.noreply.github.com>
2026-02-25 07:32:07 -0800

    Create train_service.py

---
+++ train_service.py
@@ -0,0 +1,12 @@
+# A train service system.
+# Created by Ezaiah Akyeem / Eazythedev / Erujine
+# Date Created: 02-24-26
+
+jp_train_ticket_name = input("Please enter your name, then press the 'enter' key: ")
+print(f"Welcome, {jp_train_ticket_name}, to the Hayaku Train Service.")
+ques1 = input("What country do you reside in? Please press the 'enter' key after answering: ")
+ques2 = input("Which Japanese prefecture do you reside in (If you do not reside in Japan, enter 'None'? Please press the 'enter' key after answering: ")
+ques3 = input("What train station do you want to arrive at? Please press the 'enter' key after answering: ")
+ques4 = input("What class of seat do you prefer? Economy, Premium, or Luxury? Please press the 'enter' "
+              "key after answering: ")
+ques5 = input("Which type of seat do you prefer? Aisle, or window seat? Please press the 'enter' key after answering: ")
